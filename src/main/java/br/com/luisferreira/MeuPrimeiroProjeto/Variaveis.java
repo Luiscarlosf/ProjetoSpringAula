f47a50713853273a667689869e4eb4a6bdb37f0a
Luiscarlos02 <luiscarlos02.neto@hotmail.com>
2023-06-28 22:53:19 -0300

    Segundo arquivo de variáveis modificado

--- src/main/java/br/com/luisferreira/MeuPrimeiroProjeto/Variaveis.java
+++ src/main/java/br/com/luisferreira/MeuPrimeiroProjeto/Variaveis.java
@@ -5,5 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Variaveis {
-    
+    String nomePessoaVariavel = "Luis Carlos Ferreira Neto";
+    int numerosParaSorteio[] = {1,5,9,12,46,85};
+    @GetMapping("teste/retornaVariavel")
+    public String retornaVariavel(){
+        return nomePessoaVariavel;
+    }
+
+    @GetMapping("teste/retornaArray")
+    public int retornaArray(){
+        return numerosParaSorteio[2];
+    }
 }
